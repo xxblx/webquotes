c6eb4f21e4ae42305b7e850459b996d06511000d
Oleg Kozlov <xxblx@posteo.org>
2019-03-17 12:39:07 +0100

    Print  quotes at home pages
    By default the app prints last 100 quotes. It's possible to configure number of quotes for print and an offset by parameters like 'app/?num=30&offset=100', in this example the app skips last 100 rows and shows 101-130 rows.

--- webquotes/handlers/home.py
+++ webquotes/handlers/home.py
@@ -1,11 +1,34 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+
 import tornado.web
 
 from .base import WebAuthHandler
+from ..sql.select import SelectQueries
 
 
 class HomeHandler(WebAuthHandler):
     @tornado.web.authenticated
-    def get(self):
-        self.render('home.html')
+    async def get(self):
+        offset = int(self.get_argument('offset', 0))
+        num = int(self.get_argument('num', 100))
+        if num > 100 or num < 0:
+            num = 100
+
+        async with self.db_pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute(SelectQueries.quotes, (offset, num))
+                _res = await cur.fetchall()
+
+        quotes_data = []
+        for item in _res:
+            item = list(item)
+            item[3] = datetime.utcfromtimestamp(item[3])
+            if item[4][0] is None:
+                item[4] = None
+            quotes_data.append(item)
+
+        next_offset = offset+num
+        self.render('home.html', quotes_data=quotes_data, quotes_num=num,
+                    next_offset=next_offset)
--- webquotes/sql/select.py
+++ webquotes/sql/select.py
@@ -10,3 +10,50 @@ FROM
 WHERE
     username = %s    
 """
+
+    # WITH at the beginning is needed for limiting internal rows selection
+    # from tags and quotes_tags tables, e.g. for selecting only tags used
+    # with this quotes
+    quotes = """
+WITH quotes_ids AS (
+    SELECT
+        quote_id
+    FROM
+        quotes
+    ORDER BY
+        datetime DESC
+    OFFSET %s ROWS
+    FETCH FIRST %s ROWS ONLY
+)
+
+SELECT
+    q.quote_id, 
+    quote_title, 
+    quote, 
+    datetime, 
+    array_agg(to_json(t))
+FROM
+    quotes q
+    
+    INNER JOIN quotes_ids qi
+        ON q.quote_id = qi.quote_id
+    
+    LEFT JOIN
+    (SELECT
+        tq.quote_id, 
+        tq.tag_id,
+        t.tag_name
+    FROM
+        tags_quotes tq 
+        INNER JOIN tags t
+            ON tq.tag_id = t.tag_id
+        
+        INNER JOIN quotes_ids qi
+            ON tq.quote_id = qi.quote_id
+    ) t
+        ON q.quote_id = t.quote_id
+GROUP BY
+    q.quote_id
+ORDER BY
+    q.datetime DESC
+    """
